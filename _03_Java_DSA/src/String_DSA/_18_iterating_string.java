15cda0f093a5ae06ad600de78303412df74d636e
Nandan G N <gnnandan7@gmail.com>
2022-09-14 21:30:40 +0530

    New Problems Added

---
+++ _03_Java_DSA/src/String_DSA/_18_iterating_string.java
@@ -0,0 +1,16 @@
+package String_DSA;
+
+public class _18_iterating_string
+{
+    public static void main(String[] args)
+    {
+        String s  = "Nandan";
+
+        for(int i=0;i<s.length();i++)
+        {
+            System.out.print(s.charAt(i)+" ");
+        }
+    }
+}
+//output
+//N a n d a n
