80a52de7499fbc3107ed1ca1da2acfc883dff737
Nandan G N <gnnandan7@gmail.com>
2022-09-14 21:28:09 +0530

    New Problems Added

--- _03_Java_DSA/src/String_DSA/_13_Digit_Check_In_String.java
+++ _03_Java_DSA/src/String_DSA/_13_Digit_Check_In_String.java
@@ -4,34 +4,35 @@ public class _13_Digit_Check_In_String
 {
     public static void main(String[] args)
     {
-        String s = "C123urious$^%^&Deve1234lopers";
+        String s = "0";
+
         char c[] = s.toCharArray();
 
         StringBuilder digits = new StringBuilder();
         StringBuilder symbols = new StringBuilder();
         StringBuilder validMessage = new StringBuilder();
 
-        for(char ch: c)
+        for (char ch : c)
         {
             //logic extracts the digit
-            if(Character.isDigit(ch))
+            if (Character.isDigit(ch))
             {
                 digits.append(ch);
             }
 
             //logic extracts the special symbols
-            if (!Character.isAlphabetic(ch)&&!Character.isDigit(ch))
+            if (!Character.isAlphabetic(ch) && !Character.isDigit(ch))
             {
                 symbols.append(ch);
             }
 
-            if(Character.isAlphabetic(ch))
+            if (Character.isAlphabetic(ch))
             {
                 validMessage.append(ch);
             }
         }
-        System.out.println("The digits in the string: "+digits);
-        System.out.println("The special symbols in the string: "+symbols);
-        System.out.println("The Valid message in the string: "+validMessage);
+        System.out.println("The digits in the string: " + digits);
+        System.out.println("The special symbols in the string: " + symbols);
+        System.out.println("The Valid message in the string: " + validMessage);
     }
 }
